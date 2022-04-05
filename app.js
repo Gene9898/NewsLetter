7c357d00bd56f062c10da232c5b994c5029c7800
Eugene Lum <eugenelumtf@gmail.com>
2022-04-05 17:24:21 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+  const email = req.body.email;
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+
+  const data = {
+    members : [
+      {
+        email_address : email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME : firstname,
+          LNAME : lastname
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us14.api.mailchimp.com/3.0/lists/d86471d8d1";
+
+  const options = {
+    method: "POST",
+    auth: "gene1:cb7090d4243c5db2676d91a2ce617991-us14"
+  }
+  const request = https.request(url,options,function(response){
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    }
+    else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running on port 3000");
+});
+
+
+// API key
+// cb7090d4243c5db2676d91a2ce617991-us14
+
+// List
+// d86471d8d1
